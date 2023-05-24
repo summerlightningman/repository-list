3033edd067c174ebfbfd0c8e6f7b97eda6d65a63
Dimitriy White <oblivvion333@gmail.com>
2023-05-24 22:27:39 +0300

    Добавлены базовые стили компоненту SearchBar

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,6 +1,8 @@
-import {FC, KeyboardEventHandler} from "react";
-import useSearchQueryStore from "../../store/search-query/search-query.store.ts";
-import useRepositoryListStore from "../../store/repository-list/repository-list.store.ts";
+import {FC, KeyboardEventHandler} from 'react';
+import useSearchQueryStore from '../../store/search-query/search-query.store.ts'
+import useRepositoryListStore from '../../store/repository-list/repository-list.store.ts'
+
+import './search-bar.scss'
 
 const SearchBar: FC = () => {
     const searchQueryStore = useSearchQueryStore()
@@ -14,17 +16,19 @@ const SearchBar: FC = () => {
         if (e.key === 'Enter') repositoryListStore.fetch(searchQueryStore.name)
     }
 
-    return <div className="search-bar">
-        <input
-            className="search-bar__input"
-            type="text"
-            disabled={repositoryListStore.isLoading}
-            value={searchQueryStore.name}
-            onInput={handleInput}
-            onKeyUp={handleKeyUp}
-        />
-        <button className="search-bar__submit" disabled={repositoryListStore.isLoading}>Search</button>
-    </div>
+    return <section className="search-bar">
+        <div className="search-bar__controls">
+            <input
+                className="search-bar__input"
+                type="text"
+                disabled={repositoryListStore.isLoading}
+                value={searchQueryStore.name}
+                onInput={handleInput}
+                onKeyUp={handleKeyUp}
+            />
+            <button className="search-bar__submit" disabled={repositoryListStore.isLoading}>Search</button>
+        </div>
+    </section>
 }
 
 export default SearchBar
