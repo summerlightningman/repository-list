ac1997545035fad7e85dd4c0f4333e8f625933f9
Dimitriy White <oblivvion333@gmail.com>
2023-05-25 01:17:56 +0300

    Первые стили компоненту List

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,10 +1,15 @@
-import {FC} from "react";
-import SearchBar from "@components/search-bar/search-bar.tsx";
-import Paginator from "@components/paginator/paginator.tsx";
+import { FC } from 'react';
+
+import SearchBar from '@components/search-bar/search-bar.tsx'
+import RepoList from '@components/repo-list/repo-list.tsx'
+import Paginator from '@components/paginator/paginator.tsx'
+
+import './list.scss'
 
 const List: FC = () => {
     return <div className="list">
         <SearchBar/>
+        <RepoList/>
         <Paginator/>
     </div>
 }
