37f7b42aafe2892eb4fbf46ea66d0696bdb7ec70
Dimitriy White <oblivvion333@gmail.com>
2023-05-24 21:47:03 +0300

    Рефакторинг

--- src/store/repository-list/repository-list.store.ts
+++ src/store/repository-list/repository-list.store.ts
@@ -5,8 +5,8 @@ import {RepositoryListState} from "./types.ts";
 const useRepositoryListStore = create<RepositoryListState>()(set => ({
     list: [],
     fetch: async name => {
-        const raw = await repositoryAPI.get({ name })
-        const list = raw.data.search.edges.map(({ node }) => ({
+        const { data } = await repositoryAPI.get({ name })
+        const list = data.search.edges.map(({ node }) => ({
             name: node.name,
             lastCommittedBy: new Date(node.pushedAt).toLocaleString(),
             starsCount: node.stargazerCount,
