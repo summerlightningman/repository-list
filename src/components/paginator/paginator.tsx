04195d4d3a0b31c9359bc70b66d30e3d563c9eca
Dimitriy White <oblivvion333@gmail.com>
2023-05-24 22:38:30 +0300

    Добавлен компонент paginator

---
+++ src/components/paginator/paginator.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react'
+
+const Paginator: FC = () => {
+    return <section className="paginator"></section>
+}
+
+export default Paginator
--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,9 +1,11 @@
 import {FC} from "react";
 import SearchBar from "../../components/search-bar/search-bar.tsx";
+import Paginator from "../../components/paginator/paginator.tsx";
 
 const List: FC = () => {
     return <div className="list">
         <SearchBar/>
+        <Paginator/>
     </div>
 }
 
