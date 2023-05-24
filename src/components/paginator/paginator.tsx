f7afe919f361e24fbc56cbf7af8516ef1c21f17c
Dimitriy White <oblivvion333@gmail.com>
2023-05-25 00:28:34 +0300

    Доработана пагинация

--- src/components/paginator/paginator.tsx
+++ src/components/paginator/paginator.tsx
@@ -1,17 +1,35 @@
 import { FC } from 'react'
 import useSearchQueryStore from '@store/search-query/search-query.store';
 
-const Paginator: FC = () => {
-    const searchQueryStore = useSearchQueryStore()
+import './paginator.scss'
 
-    const goToPage = (page: number) => () => searchQueryStore.setPage(page)
+const Paginator: FC = () => {
+    const { page, setPage } = useSearchQueryStore()
+
+    const goToPage = (page: number) => () => setPage(page)
+    const goToPrevPage = () => page > 1 && setPage(page - 1)
+    const goToNextPage = () => page < 10 && setPage(page + 1)
+    const getButtonClasses = (pageNum: number) => {
+        const classes = ['paginator__item']
+        if (pageNum === page) classes.push('paginator__item--active')
+        return classes.join(' ')
+    }
 
     return <section className="paginator">
+        <button className="paginator__item" onClick={goToPrevPage} key={-1}>◀︎</button>
         {
             Array(10).fill(null).map(
-                (_, idx) => <button onClick={goToPage(idx + 1)} key={idx}>{idx + 1}</button>
+                (_, idx) =>
+                    <button
+                        className={getButtonClasses(idx + 1)}
+                        onClick={goToPage(idx + 1)}
+                        key={idx}
+                    >
+                        {idx + 1}
+                    </button>
             )
         }
+        <button className="paginator__item" onClick={goToNextPage} key={11}>▶︎</button>
     </section>
 }
 
