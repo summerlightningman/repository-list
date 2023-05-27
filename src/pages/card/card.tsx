7d4dcd9892bae9e3fbffaf3b327626d63669bada
Dimitriy White <oblivvion333@gmail.com>
2023-05-28 00:58:32 +0300

    Оформлена стилизация компонента Card (кроме инфо о пользователе)

--- src/pages/card/card.tsx
+++ src/pages/card/card.tsx
@@ -1,24 +1,61 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect} from "react";
-import {RouteName} from "../../../router.tsx";
-import useRepositoryCardStore from "@store/repository-card/repository-card.ts";
+import {useNavigate, useParams} from 'react-router-dom'
+import {useEffect} from 'react'
+import {RouteName} from '../../../router.tsx'
+import useRepositoryCardStore from '@store/repository-card/repository-card.ts'
+
+import './card.scss'
 
 const Card = () => {
     const navigate = useNavigate()
     const { name, owner } = useParams()
-    const { isLoading, fetch: getRepositoryInfo } = useRepositoryCardStore()
+    const {
+        isLoading,
+        fetch: getRepositoryInfo,
+        owner: {avatarUrl, url, login},
+        name: repoName,
+        description,
+        starsCount,
+        languageCount,
+        languageList
+    } = useRepositoryCardStore()
 
     useEffect(() => {
         if (!name || !owner) {
             navigate(RouteName.Search)
             return
         }
-
         getRepositoryInfo({ name, owner })
     }, [name, owner, getRepositoryInfo, navigate])
 
+    const goBack = () => navigate(-1)
+
     return <main className="card">
-        {isLoading ? 'Loading...' : 'LOADED!'}
+        {
+            isLoading
+            ? 'Loading...'
+                : <div className="card__repo-info">
+                    <button className="card__back-btn" onClick={goBack}>←<br/>Go back</button>
+                    <section className="card__repo-section">
+                        <div className="flex-space-between">
+                            <h1 className="repo-name">{repoName}</h1>
+                            <span className="stars-count">{starsCount}</span>
+                        </div>
+                        {
+                            languageCount && <p className="language-list">
+                            Languages: {
+                                languageList.map(lang => <span className="language-list__item">{lang}</span>)
+                            }
+                        </p>
+                        }
+                        <p className="card__repo-description">{description || '[no description]'}</p>
+                    </section>
+                    <img src={avatarUrl} alt={url} className="card__avatar"/>
+                    <section className="card__user-info">
+                        <p>{login}</p>
+                        <a href={url} target="_blank">{url}</a>
+                    </section>
+                </div>
+        }
     </main>
 }
 
