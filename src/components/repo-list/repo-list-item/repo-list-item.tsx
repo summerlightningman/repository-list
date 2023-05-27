32a69206eb927c3c10c00c91580670c760f0b475
Dimitriy White <oblivvion333@gmail.com>
2023-05-27 10:25:17 +0300

    Переписан логика размеров элементов

--- src/components/repo-list/repo-list-item/repo-list-item.tsx
+++ src/components/repo-list/repo-list-item/repo-list-item.tsx
@@ -1,7 +1,6 @@
 import {FC} from 'react'
 import {RepoListItemProps} from '@components/repo-list/repo-list-item/repo-list-item.types.ts'
 
-
 const RepoListItem: FC<RepoListItemProps> = ({name, starsCount, lastCommittedBy, link}) => {
     return <li className="repo-list__item">
         <span className="name">{name}</span>
