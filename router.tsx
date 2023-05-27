9edf2556af58fb110de3be04a7dc9c6179805e1f
Dimitriy White <oblivvion333@gmail.com>
2023-05-27 12:57:08 +0300

    router вынесен в отдельный файл

---
+++ router.tsx
@@ -0,0 +1,26 @@
+import {createBrowserRouter} from 'react-router-dom'
+import App from './src/app'
+import List from './src/pages/list/list'
+import Card from './src/pages/card/card'
+
+enum RouteName {
+    Search = '/search',
+    RepositoryCard = '/repository'
+}
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children: [
+            {
+                path: RouteName.Search,
+                element: <List/>
+            },
+            {
+                path: `${RouteName.RepositoryCard}/:owner/:name`,
+                element: <Card/>
+            }
+        ]
+    }
+])
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {RouterProvider} from 'react-router-dom';
 
-import App from './app.tsx'
-import List from '@pages/list/list.tsx';
-import Card from '@pages/card/card.tsx';
 
+import {router} from "../router.tsx";
 import './main.scss'
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App/>,
-        children: [
-            {
-                path: '/search',
-                element: <List/>
-            },
-            {
-                path: '/repository/:id',
-                element: <Card/>
-            }
-        ]
-    }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
